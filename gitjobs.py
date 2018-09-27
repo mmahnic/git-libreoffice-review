039a3a4289aebef31e0d046bc8ef55b097f692ca
Marko Mahnič <marko.mahnic@gmail.com>
2018-09-27 10:38:41 +0200

    Remove lines containing '->' when creating a branch list

--- gitjobs.py
+++ gitjobs.py
@@ -7,10 +7,16 @@ encoding = "utf-8"
 def getBranches( gitroot ):
     cmd = ["git", "branch", "-a"]
     cwd = os.getcwd()
+
+    def clean( branch ):
+        if branch.find("->") > 0:
+            return u""
+        return branch.rstrip()
+
     try:
         os.chdir( gitroot )
         out = subp.check_output( cmd ).decode( encoding, "replace" )
-        branches = [ line for line in out.split( "\n" ) if len(line.strip()) > 0 ]
+        branches = [ clean(line) for line in out.split( "\n" ) if len(clean(line)) > 0 ]
         curbranch = u""
         for branch in branches:
             if branch.startswith( "*" ):
@@ -18,7 +24,6 @@ def getBranches( gitroot ):
         branches = [b[1:].strip() for b in branches]
         return (branches, curbranch)
     except Exception as e:
-        difftext += ["**Error**: %s" % e, ""]
         print(e) # TODO: send to UI
 
     os.chdir( cwd )
