658722cbb0e14e574e4867cfd0ea06a000bc4989
Marko Mahnič <marko.mahnic@gmail.com>
2018-11-23 08:25:49 +0100

    Display author instead of committer name in overview

--- generator.py
+++ generator.py
@@ -109,7 +109,7 @@ class DiffGenerator:
 class OverviewGenerator:
     def __init__(self, settings):
         self.settings = settings
-        self.logFormat = '''--pretty=format:$%m$%H %ai %cn%n%B'''
+        self.logFormat = '''--pretty=format:$%m$%H %ai %an%n%B'''
         self.merges = "--no-merges"
 
 
