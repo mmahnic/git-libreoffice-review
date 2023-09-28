d42c522c6092249f0427b802499d2fc9763c3a73
Marko Mahnič <marko.mahnic@gmail.com>
2023-09-28 19:26:37 +0200

    Build using a virtual environment

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [[ ! -d xtemp/.venv ]]; then
+   py -m venv xtemp/.venv
+   source xtemp/.venv/Scripts/activate
+   # Use an older version that does not scare Windows Defender.
+   pip install pyinstaller==5.13.2
+fi
+
+source xtemp/.venv/Scripts/activate
+
 opts="--noconsole --onefile"
 # opts="$opts --log-level=TRACE"
 
@@ -11,9 +20,9 @@ else
    sep=";"
 fi
 pyinstaller $opts \
-   --specpath="xdata" \
-   --workpath="xdata/build" \
-   --distpath="xdata/dist" \
+   --specpath="xtemp" \
+   --workpath="xtemp/build" \
+   --distpath="xtemp/dist" \
    --add-data="../template/codereview001_tmpl.odt${sep}template" \
    --add-data="../template/codereview002_tmpl.odt${sep}template" \
    gitreviewdoc.py
